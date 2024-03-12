449448b507b7f668ccc94818feefddb706861932
kajalasodiya47 <140589136+kajalasodiya47@users.noreply.github.com>
2024-03-12 13:55:45 +0530

    Delete myapp directory

--- myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import *
-
-# Register your models here.
-
-# register product model here
-admin.site.register(ProductMst)
-# register product sub category model here
-admin.site.register(ProductSubCat)
